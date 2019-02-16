85364c982734fd53f67c82051a3a27268674718b
DhruvilPatel <atdhruvilpatel@gmail.com>
2019-02-16 16:45:51 -0500

    added some code

---
+++ lib/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const orange = const Color(0xFFf76b3a);
+const orange_light = const Color(0xFFf7aa3a);
+const orange_dark = const Color(0xFF562d03);
+const grey_light = const Color(0xFF8e857c);
+const grey_dark = const Color(0xFF3b3733);
+
+const white = Colors.white;
+const black = Colors.black;
---
+++ lib/screens/mybudget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:fiserv_valet/colors.dart';
+
+class MyBudget extends StatelessWidget {
+  @override
+  Widget build (BuildContext context) => new Scaffold(
+
+    //App Bar
+//    appBar: new AppBar(
+//      backgroundColor: bluegrey,
+//      title: new Text(
+//        'About HackRU',
+//        style: new TextStyle(
+//          fontSize: Theme.of(context).platform == TargetPlatform.iOS ? 17.0 : 20.0,
+//        ),
+//      ),
+//      elevation: 0.3,
+//    ),
+
+    //Content of tabs
+    body: new PageView(
+      children: <Widget>[
+        new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('My Budget Page')
+          ],
+        )
+      ],
+    ),
+  );
+}
